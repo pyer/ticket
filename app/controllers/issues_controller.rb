68c31843c0696e177f2c05407eeca862c3e484f1
Pierre <pierre.bazonnard@gmail.com>
2015-09-17 22:08:58 +0200

    * issues temporary refactoring

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -1,19 +1,20 @@
 class IssuesController < ApplicationController
   skip_before_filter  :verify_authenticity_token
-  
+
   def index
     @sub_title = "List of issues"
     @add_item  = '<a href="/issues/new">New issue</a>'
+    #@issues    = Issue.order(id).all
     @issues    = Issue.all
   end
-  
+
   def new
     @sub_title = "New issue"
+    @action = "/issue/create"
   end
-  
+
   def create
     logger.debug "--> Creating new issue"
-#Create new issue : {"project"=>"aze", "subject"=>"aaaaaaaaaaaaaaaaaaaa", "description"=>"aze description", "author"=>"", "controller"=>"issues", "action"=>"create"}
     issue = Issue.new
     #issue.project = Project.id(params[:project])
     issue.project_id = 1
@@ -22,8 +23,24 @@ class IssuesController < ApplicationController
     issue.author  = User.current.name
     issue.status  = 'To do'
     issue.save
-    logger.debug "--> Issue #{issue.subject} is created"
     redirect_to issues_url
   end
-  
+
+  def edit
+    current    = params[:id]
+    @sub_title = "Edit issue ##{current}"
+    @action    = "/issue/update?id=#{current}"
+  end
+
+  def update
+    current    = params[:id]
+    logger.debug "--> Updating issue #{current}"
+    issue = Issue.find(current.to_i)
+    issue.subject = params[:subject]
+    issue.description = params[:description]
+    issue.owner = User.current.name
+    issue.save
+    redirect_to issues_url
+  end
+
 end

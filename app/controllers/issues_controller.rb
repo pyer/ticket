47e442ada5e215f97c425667bad3e4b9542464c6
Pierre <pierre.bazonnard@gmail.com>
2015-09-20 22:28:09 +0200

    + issue new and edit

--- app/controllers/issues_controller.rb
+++ app/controllers/issues_controller.rb
@@ -4,42 +4,40 @@ class IssuesController < ApplicationController
   def index
     @sub_title = "List of issues"
     @add_item  = '<a href="/issues/new">New issue</a>'
-    #@issues    = Issue.order(id).all
     @issues    = Issue.all
+    #@issues    = Issue.order(:created_on).all
   end
 
   def new
     @sub_title = "New issue"
-    @action = "/issue/create"
+    @project, @description, @status = Issue.new.default_values
   end
 
   def create
-    logger.debug "--> Creating new issue"
     issue = Issue.new
-    #issue.project = Project.id(params[:project])
-    issue.project_id = 1
-    issue.subject = params[:subject]
-    issue.description = params[:description]
-    issue.author  = User.current.name
-    issue.status  = 'To do'
+    issue.i_create(params[:project], params[:description], params[:status])
     issue.save
     redirect_to issues_url
   end
 
   def edit
-    current    = params[:id]
-    @sub_title = "Edit issue ##{current}"
-    @action    = "/issue/update?id=#{current}"
+    @current_id = params[:id]
+    @sub_title  = "Edit issue ##{current}"
+    issue = Issue.find(@current_id.to_i)
+    if issue.nil?
+      @project, @description, @status = Issue.new.default_values
+    else
+      @project, @description, @status = issue.current_values
+    end
   end
 
   def update
-    current    = params[:id]
-    logger.debug "--> Updating issue #{current}"
-    issue = Issue.find(current.to_i)
-    issue.subject = params[:subject]
-    issue.description = params[:description]
-    issue.owner = User.current.name
-    issue.save
+    @current_id = params[:id]
+    issue = Issue.find(@current_id.to_i)
+    if !issue.nil?
+      issue.i_update(params[:project], params[:description], params[:status])
+      issue.save
+    end
     redirect_to issues_url
   end
 

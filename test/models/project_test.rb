f21400cdc90841c731028d46099922be0f4b1633
Pierre <pierre.bazonnard@gmail.com>
2015-09-22 22:02:50 +0200

    + created_on and updated_on are not nil

--- test/models/project_test.rb
+++ test/models/project_test.rb
@@ -35,20 +35,20 @@ class ProjectTest < ActiveSupport::TestCase
     user = User.try_to_login('pba','pba')
     project = Project.new
     project.p_create('Name','Description')
-#    assert_equal project.created_on
-    assert_equal project.created_by, 'pba'
-    assert_nil   project.updated_on
-    assert_nil   project.updated_by
+    assert_not_nil project.created_on
+    assert_equal   project.created_by, 'pba'
+    assert_nil     project.updated_on
+    assert_nil     project.updated_by
   end
 
   test "check update project" do
     user = User.try_to_login('pba','pba')
     project = Project.new
     project.p_update('Name','Description')
-    assert_nil   project.created_on
-    assert_nil   project.created_by
-#    assert_nil   project.updated_on
-    assert_equal project.updated_by, 'pba'
+    assert_nil     project.created_on
+    assert_nil     project.created_by
+    assert_not_nil project.updated_on
+    assert_equal   project.updated_by, 'pba'
   end
 
 end

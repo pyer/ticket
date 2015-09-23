d7aee261f7e6c4f64384d0708e8b1d553cad21c1
Pierre <pierre.bazonnard@gmail.com>
2015-09-23 16:22:27 +0200

    + more tests for projects controller

--- test/controllers/projects_controller_test.rb
+++ test/controllers/projects_controller_test.rb
@@ -6,5 +6,28 @@ class ProjectsControllerTest < ActionController::TestCase
     assert_response :success
   end
 
-  
+  test "should get a new project" do
+    get :new
+    assert_response :success
+  end
+
+  test "should create a project" do
+    user = User.try_to_login('pba','pba')
+    n = Project.count
+    post :create, { :name => 'new', :description => 'project' }
+    assert_redirected_to projects_url
+    assert_equal (n+1), Project.count
+  end
+
+  test "should edit a project" do
+    get :edit, { :id => '1' }
+    assert_response :success
+  end
+
+  test "should update a project" do
+    user = User.try_to_login('pba','pba')
+    post :update, { :id => '1', :name => 'update', :description => 'project' }
+    assert_redirected_to projects_url
+  end
+
 end

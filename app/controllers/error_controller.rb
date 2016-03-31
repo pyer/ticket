144a762e85ed15cfc01564d17d98fc8ca8a60f51
Pierre <pierre.bazonnard@gmail.com>
2016-03-31 22:29:58 +0200

    - useless comments

--- app/controllers/error_controller.rb
+++ app/controllers/error_controller.rb
@@ -2,10 +2,5 @@ class ErrorController < ApplicationController
   def not_found
     logger.error "Error 404: Page not found"
     render :template => "error/not_found", :layout => false, :status => 404
-#    respond_to do |format|
-#      format.html { render status: 404 }
-#    end
-#  rescue ActionController::UnknownFormat
-#      render status: 404, text: "Unknown !"
   end
 end

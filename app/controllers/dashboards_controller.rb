46316ad3ff977fa97b22ebc39fd15160b60bb988
pyer <pierre.bazonnard@gmail.com>
2024-11-08 17:48:57 +0100

    Tha dashboard is the home page

--- app/controllers/dashboards_controller.rb
+++
@@ -1,7 +0,0 @@
-class DashboardsController < ApplicationController
-  def index
-    project_name, project_desc = Project.current_values(params[:project])
-    @sub_title = "Dashboard of project #{project_name}"
-  end
- 
-end

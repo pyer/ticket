9187e426f013a5fac00a30fd4ff41f54deb4f282
pyer <pierre.bazonnard@gmail.com>
2024-11-11 11:34:25 +0100

    Add 'RAILS_ENV' in config

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# ENV['RAILS_ENV'] = 'development'
+ENV['RAILS_ENV'] = 'production'
+
 module Ticket
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
--- lib/tasks/run.rake
+++ lib/tasks/run.rake
@@ -1,4 +1,4 @@
 desc "Run Rails server"
-task :run => [:cleanup ] do
-  system "rackup -p 8080"
+task :run => [:cleanup] do
+  system "rails server"
 end
